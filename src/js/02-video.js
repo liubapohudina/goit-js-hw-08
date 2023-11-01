464f9426acc25bcc69f708cee155d9813886ba03
Liubov <drogaltseva92@gmail.com>
2023-11-01 14:50:38 +0100

    update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
 
+const videoContainer = document.getElementById('vimeo-player');
+
+const idPlayer = new Player(videoContainer);
+
+idPlayer.on('touchstart', function(event) {
+    event.preventDefault();
+}, { passive: false });
+
+idPlayer.on('timeupdate', function(data) {
+    const currentTime = data.seconds;
+    console.log(localStorage.idPlayer = { "videoplayer-current-time": currentTime });
+
+    idPlayer.setCurrentTime(currentTime)
+        .then(function(seconds) {
+            const actualTime = seconds;
+            console.log('Current time set to', actualTime);
+        })
+        .catch(function(error) {
+            switch (error.name) {
+                case 'RangeError':
+                    console.error('The time was out of range (less than 0 or greater than the video’s duration).');
+                    break;
+                default:
+                    console.error('Some other error occurred:', error);
+                    break;
+            }
+        });
+});
